96d06ba202a52bb2c2c94ce7a99164208a9f8a6c
Christofer Castillo <ccastillo993a@gmail.com>
2022-10-24 12:53:58 -0500

    refactor: se cambia la estructura para las imágenes

--- src/controllers/planets.ts
+++ src/controllers/planets.ts
@@ -5,7 +5,6 @@ export const getFindOne = async (req: Request, res: Response) => {
 	const { name } = req.params
 	const planet = await planetService.getPlanet(name)
 
-	if (!planet) return res.status(404).send(`Not found this planet: ${name}`)
-
+	if (!planet) return res.status(404).json(`Not found this planet: ${name}`)
 	return res.json(planet)
 }
